2ec7d8b97a85fe7f6cab4f0ee920f5b33238c39b
Danielle Ellis <thedanielleellis@gmail.com>
2020-04-21 05:01:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,7 +19,7 @@ end
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3,}
 
-  shipping_manifest["muskets"] #=> 2
-  shipping_manifest["gun powder"] #=>4
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
   shipping_manifest
 end
