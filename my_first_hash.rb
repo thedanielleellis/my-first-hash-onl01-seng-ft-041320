846f3a36289769dbdc8378df8e1286f235ef8653
Danielle Ellis <thedanielleellis@gmail.com>
2020-04-21 04:39:15 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,29 +1,24 @@
 def my_hash
-  my_hash = {"name" => "Danielle", "age" => 30, "city" = "Memphis"}
+  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  my_hash = {"name" => "Alexis", "age" => 26, "location" => "LA"}
 end
 
 
 def shipping_manifest
- the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3} 
+  #set a variable called `shipping_manifest`, equal to a hash
+  #fill that hash with key/value pairs that describe the following information:
+  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   shipping_manifest["oil paintings"]
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
- 
-shipping_manifest["muskets"] #=> 2 
-shipping_manifest["gun powder"] #=> 4
-shipping_manifest
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+
+  shipping_manifest["pearl necklace"] = 1
+  shipping_manifest
 end
