ad357914f2b64b2b80fdaff0bc616b3bbdc01d5a
Danielle Ellis <thedanielleellis@gmail.com>
2020-04-21 04:51:06 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,8 +17,8 @@ def retrieval
 end
 
 def adding
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3, "muskets" => 2, "gun powder" => 4}
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3,}
 
-  shipping_manifest["pearl necklace"] = 1
+  shipping_manifest["muskets" =>2, "gun powder" =>4]
   shipping_manifest
 end
